1471fd68e72616954da713f439d740d5bc505cb5
zabavsky.alex <zabavsky.alex@mail.ru>
2018-02-21 00:08:59 +0300

    quadratic-equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+    var arr_rez = [];
+    var arr = equation.split(' ');
+    var a = parseInt(arr[0]);
+    var b = parseInt(arr[4]);
+    if(arr[3] == '-') b*=-1;
+    var c = parseInt(arr[8]);
+    if(arr[7] == '-') c*=-1;
+    var d = Math.pow(b, 2) - 4*a*c;
+
+    if (d>=0){
+        arr_rez.push(Math.round((b*-1 + Math.sqrt(d))/(2*a)));
+        if (d>0){
+            arr_rez.push(Math.round((b*-1 - Math.sqrt(d))/(2*a)));
+        };
+    };
+
+    return arr_rez.sort(function sor_arr(a, b) { return a - b; });
+};
